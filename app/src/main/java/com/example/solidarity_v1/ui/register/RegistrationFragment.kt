f2905b4bedaeea491eaea14326831d6af33e6bba
mcicishvilii <mcicishvilii@gmail.com>
2022-12-15 01:54:18 +0400

    register page done. needs to impelement sending and verifying code

--- app/src/main/java/com/example/solidarity_v1/ui/register/RegistrationFragment.kt
+++ app/src/main/java/com/example/solidarity_v1/ui/register/RegistrationFragment.kt
@@ -8,11 +8,22 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import androidx.core.widget.doOnTextChanged
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import com.example.solidarity_v1.R
 import com.example.solidarity_v1.common.BaseFragment
 import com.example.solidarity_v1.databinding.FragmentRegistrationBinding
+import kotlinx.coroutines.launch
 
-class RegistrationFragment : BaseFragment<FragmentRegistrationBinding>(FragmentRegistrationBinding::inflate),AdapterView.OnItemSelectedListener {
+class RegistrationFragment :
+    BaseFragment<FragmentRegistrationBinding>(FragmentRegistrationBinding::inflate),
+    AdapterView.OnItemSelectedListener {
+
+    private val vm: RegistrationViewModel by viewModels()
 
     var priority = ""
     override fun viewCreated() {
@@ -20,7 +31,9 @@ class RegistrationFragment : BaseFragment<FragmentRegistrationBinding>(FragmentR
     }
 
     override fun listeners() {
-
+        checkVisibility()
+        makeSelectionGray()
+        getCode()
     }
 
     private fun setupSpinner() {
@@ -33,6 +46,7 @@ class RegistrationFragment : BaseFragment<FragmentRegistrationBinding>(FragmentR
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         binding.countryCodes.adapter = adapter
         binding.countryCodes.onItemSelectedListener = this
+
     }
 
     override fun onNothingSelected(parent: AdapterView<*>?) {
@@ -44,4 +58,41 @@ class RegistrationFragment : BaseFragment<FragmentRegistrationBinding>(FragmentR
 
     }
 
+    private fun checkVisibility() {
+        binding.etPhoneNumber.doOnTextChanged { text, start, before, count ->
+            binding.btnNext.setBackgroundResource(R.drawable.button_outline_blue)
+        }
+    }
+
+    private fun makeSelectionGray() {
+        binding.btnEnglish.setOnClickListener {
+            binding.btnEnglish.setBackgroundResource(R.drawable.button_outline_gray)
+            binding.btnGeorgian.setBackgroundResource(R.drawable.button_outline_blue)
+            binding.btnUkrainian.setBackgroundResource(R.drawable.button_outline_blue)
+        }
+
+        binding.btnGeorgian.setOnClickListener {
+            binding.btnEnglish.setBackgroundResource(R.drawable.button_outline_blue)
+            binding.btnGeorgian.setBackgroundResource(R.drawable.button_outline_gray)
+            binding.btnUkrainian.setBackgroundResource(R.drawable.button_outline_blue)
+        }
+
+        binding.btnUkrainian.setOnClickListener {
+            binding.btnEnglish.setBackgroundResource(R.drawable.button_outline_blue)
+            binding.btnGeorgian.setBackgroundResource(R.drawable.button_outline_blue)
+            binding.btnUkrainian.setBackgroundResource(R.drawable.button_outline_gray)
+        }
+    }
+
+    private fun getCode() {
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                binding.btnNext.setOnClickListener {
+                    val numb = binding.etPhoneNumber.text.toString()
+                    vm.sendVerificationCode(requireActivity(),numb)
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/solidarity_v1/ui/register/RegistrationViewModel.kt
+++ app/src/main/java/com/example/solidarity_v1/ui/register/RegistrationViewModel.kt
@@ -1,7 +1,55 @@
 package com.example.solidarity_v1.ui.register
 
+import android.app.Activity
+import android.content.Context
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import java.util.concurrent.TimeUnit
 
 class RegistrationViewModel : ViewModel() {
 
+    lateinit var storedVerificationId: String
+    lateinit var resendToken: PhoneAuthProvider.ForceResendingToken
+    private lateinit var mauth: FirebaseAuth
+
+
+    val callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+            Log.d("GFG", "onVerificationCompleted Success")
+        }
+
+        override fun onVerificationFailed(e: FirebaseException) {
+            Log.d("GFG", "onVerificationFailed  $e")
+        }
+        override fun onCodeSent(
+            verificationId: String,
+            token: PhoneAuthProvider.ForceResendingToken,
+        ) {
+
+            storedVerificationId = verificationId
+            resendToken = token
+            Log.d("GFG", "onCodeSent: $verificationId")
+            Log.d("GFG", "onCodeSent: $token")
+        }
+    }
+
+    fun sendVerificationCode(context: Activity,num:String) {
+        mauth = Firebase.auth
+        val options = PhoneAuthOptions.newBuilder(mauth)
+            .setPhoneNumber(num) // Phone number to verify
+            .setTimeout(120L, TimeUnit.SECONDS) // Timeout and unit
+            .setActivity(context) // Activity (for callback binding)
+            .setCallbacks(callbacks) // OnVerificationStateChangedCallbacks
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
+        Log.d("GFG" , "Auth started")
+    }
 }
